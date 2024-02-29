1cd14179ca7f269add7157312f5f8c0477cdec9a
Tai <taiseicee@gmail.com>
2024-02-28 19:59:34 -0500

    Added Ball Kick Logic

--- Untitled Goose Game 2D/Assets/Scripts/Ball/Ball.cs
+++ Untitled Goose Game 2D/Assets/Scripts/Ball/Ball.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Ball : MonoBehaviour {
+    [SerializeField] private CircleCollider2D colliderPlayer;
+    [SerializeField] private Rigidbody2D ballBody;
+    [SerializeField] private CircleCollider2D colliderBall;
+    [SerializeField] private ContactFilter2D ballKickFilter;
+    [SerializeField] private float hitBuffer = 0.05f;
+    [SerializeField] private float kickForce = 3f;
+    void Start() {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Vector3 playerPosition = colliderPlayer.transform.position;
+        Vector3 repulsionDirection = (transform.position - playerPosition).normalized;
+
+        RaycastHit2D[] raycastResults = new RaycastHit2D[1];
+        int numRaycastHits = Physics2D.CircleCast(
+            transform.position,
+            colliderBall.radius + hitBuffer,
+            repulsionDirection,
+            ballKickFilter,
+            raycastResults,
+            0f
+        );
+
+        if (numRaycastHits <= 0) return;
+        ballBody.AddForce(repulsionDirection * kickForce, ForceMode2D.Impulse);
+
     }
 }
