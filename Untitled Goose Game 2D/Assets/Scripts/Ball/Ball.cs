51b2f1b51bce26683d2d672f14cb34e308978bc3
Tai <taiseicee@gmail.com>
2024-03-01 09:10:31 -0500

    Finished Ball Bounds

--- Untitled Goose Game 2D/Assets/Scripts/Ball/Ball.cs
+++ Untitled Goose Game 2D/Assets/Scripts/Ball/Ball.cs
@@ -7,13 +7,31 @@ public class Ball : MonoBehaviour {
     [SerializeField] private Rigidbody2D ballBody;
     [SerializeField] private CircleCollider2D colliderBall;
     [SerializeField] private ContactFilter2D ballKickFilter;
+    [SerializeField] private SpriteRenderer worldBoundsObject;
     [SerializeField] private float hitBuffer = 0.05f;
     [SerializeField] private float kickForce = 3f;
-    void Start() {
+    private Bounds ballBounds;
+    private void Start() {
+        Bounds worldBounds = worldBoundsObject.bounds;
 
+        float ballRadius = transform.localScale.x / 2f;
+
+        float ballMinX = worldBounds.min.x + ballRadius;
+        float ballMaxX = worldBounds.max.x - ballRadius;
+
+        float ballMinY = worldBounds.min.y + ballRadius;
+        float ballMaxY = worldBounds.max.y - ballRadius;
+
+        ballBounds = new Bounds();
+        ballBounds.SetMinMax(
+            new Vector3(ballMinX, ballMinY, -transform.position.z),
+            new Vector3(ballMaxX, ballMaxY, transform.position.z)
+        );
     }
 
-    void Update() {
+    private void Update() {
+        StayInBounds();
+
         Vector3 playerPosition = colliderPlayer.transform.position;
         Vector3 repulsionDirection = (transform.position - playerPosition).normalized;
 
@@ -30,5 +48,12 @@ public class Ball : MonoBehaviour {
         if (numRaycastHits <= 0) return;
         ballBody.AddForce(repulsionDirection * kickForce, ForceMode2D.Impulse);
 
+    }
+
+    private void StayInBounds() {
+        if (transform.position.x < ballBounds.min.x) ballBody.AddForce(Vector2.right * kickForce, ForceMode2D.Impulse);
+        if (transform.position.x > ballBounds.max.x) {
+            ballBody.AddForce(Vector2.left * kickForce, ForceMode2D.Impulse);
+        }
     }
 }
