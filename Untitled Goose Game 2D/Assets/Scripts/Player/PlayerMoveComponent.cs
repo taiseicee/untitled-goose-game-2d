c40018bb76af9fc17edeee631aa1078c0ab98533
Tai <taiseicee@gmail.com>
2024-03-01 06:01:33 -0500

    Fixed Movement Bug

--- Untitled Goose Game 2D/Assets/Scripts/Player/PlayerMoveComponent.cs
+++ Untitled Goose Game 2D/Assets/Scripts/Player/PlayerMoveComponent.cs
@@ -99,7 +99,7 @@ public class PlayerMoveComponent : MonoBehaviour {
             Vector2.down,
             walkableFilter,
             raycastResults,
-            predictiveCollider.radius * 2f + snapBuffer
+            predictiveCollider.radius * 2f
         );
 
         if (numRaycastHits < 1) return;
